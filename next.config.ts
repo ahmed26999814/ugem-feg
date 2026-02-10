67d8f53fa42f1e8545e65f520e49c072c1500cd1
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-02-10 07:05:34 +0000

    Stabilize build tooling

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    useLightningcss: false,
+    webpackBuildWorker: false,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "motion-utils": require.resolve("motion-utils"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
